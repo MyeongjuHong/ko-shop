6670cede521a46dced851218904bd17c65a9b5cd
[Github <[Github>
2021-12-19 23:04:07 +0900

    feat: Implement site structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { React, useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Goods from "./pages/Goods";
+import Cart from "./pages/Cart";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="" exact={true} element={<Home />} />
+        <Route path="/goods" exact={true} element={<Goods />} />
+        <Route path="/cart" exact={true} element={<Cart />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/Goods.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Goods() {
+  return (
+    <div>
+      <h1>this is Goods page</h1>
+    </div>
+  );
+}
+
+export default Goods;
