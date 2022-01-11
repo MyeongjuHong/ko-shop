ea2d60c3559fed9db7496c384a98a636add8c904
[Github <[Github>
2022-01-11 16:06:37 +0900

    feat: Add Tags file codes

--- src/components/Tags.js
+++ src/components/Tags.js
@@ -4,9 +4,9 @@ import { Link } from "react-router-dom";
 function Tags() {
   return (
     <div>
-      <div># 겨울 방한템</div>
-      <div># 나를 위한 선물</div>
-      <div># 캠핑 용품</div>
+      <div> # 겨울 방한템 </div>
+      <div> # 나를 위한 선물 </div>
+      <div> # 캠핑 용품 </div>
     </div>
   );
 }
